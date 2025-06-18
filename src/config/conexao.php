7d74ad453226664541ac0a63dc0285890071c7b8
João Vitor Colombo <jvcolombo.06@gmail.com>
2025-06-18 17:59:25 -0300

    feat: colocando a conexão como função

--- src/config/conexao.php
+++ src/config/conexao.php
@@ -1,13 +1,15 @@
 <?php 
-//Conectando ao banco de dados
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$nome_banco = 'loja_loki';
+function connectBanco() {
+  $host = 'localhost';
+  $usuario = 'root';
+  $senha = '';
+  $nome_banco = 'loja_loki';
 
-$conexao = mysqli_connect($host, $usuario, $senha, $nome_banco);
+  $conexao = mysqli_connect($host, $usuario, $senha, $nome_banco);
 
-if (!$conexao) {
-    die("Connection failed: " . mysqli_connect_error());
-}  
+  if (!$conexao) {
+      die("Connection failed: " . mysqli_connect_error());
+  }  
+  return $conexao;
+}
 ?>
