027e4ab6cdf0a0600e842d422f5b21fdc286f26d
Gustavo <nevesg657@gmail.com>
2025-06-20 14:55:43 -0300

    estilizacao da pagina login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-// JS
+    const TOTAL = 70;
+    const container = document.querySelector('.chuva');
+
+    for (let i = 0; i < TOTAL; i++) {
+        const sq = document.createElement('span');
+
+        const size = Math.floor(Math.random() * 40) + 10;
+        sq.style.width = size + 'px';
+        sq.style.height = size + 'px';
+        sq.style.left = Math.random() * 100 + 'vw';
+        sq.style.animationDuration = (3 + Math.random() * 5) + 's';
+        sq.style.animationDelay = (-Math.random() * 5) + 's';
+
+        container.appendChild(sq);
+    }
--- usuarios/login.php
+++ usuarios/login.php
@@ -6,43 +6,43 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Fragmentos de Loki</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
 </head>
 <style>
-body {
-    min-height: 100vh;
-    display: flex;
-    background-color:rgb(20, 19, 19);
-}
+
 </style>
 <body>
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Login</h4>
-                </div>
-                <div class="card-body">
-                <?php include_once __DIR__ . '/../src/config/mensagem.php';?>
-                    <form action="logar.php" method="POST">
-                        <div class="mb-3">
-                            <label>Email</label>
-                            <input type="text" name="email" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <label>Senha</label>
-                            <input type="password" name="password" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <input type="submit" name="login_btn" class="btn btn-primary" value="Entrar">
-                        </div>
-                    </form>
-                    <div class="mb-3">
-                        <a href="createUser.php" class="btn btn-secondary">Cadastrar</a>
+    <div class="chuva"></div>
+    <div class="container mt-5" id="login-container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card" id="login-card">
+                    <div class="card-header">
+                        <h4>Bem Vindo, Variante!</h4>
+                    </div>
+                    <div class="card-body">
+                        <?php include_once __DIR__ . '/../src/config/mensagem.php';?>
+                        <form action="logar.php" method="POST">
+                            <div class="mb-3">
+                                <input type="text" name="email" class="form-control" required placeholder="Email AVT">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" name="password" class="form-control" required placeholder="Senha Temporal">
+                            </div>
+                            <div class="mb-3">
+                                <input type="submit" name="login_btn" class="btn btn-primary" value="Entrar">
+                            </div>
+                            <div class="mb-3">
+                                <a href="createUser.php" class="btn btn-secondary" id="btn-cadastrar">Cadastrar</a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-    </div>
-</div>
+        </div>
 </body>
+<script src="../assets/js/script.js"></script>
+</html>
