b23294c3d57b8ee110af43cdbcf412eb0e3c532f
João Vitor Colombo <jvcolombo.06@gmail.com>
2025-06-20 12:39:51 -0300

    feat: inserindo dados

--- public/navbar.php
+++ public/navbar.php
@@ -4,7 +4,7 @@ include_once '../src/config/init.php';
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Fragmentos de Loki</a>
+    <a class="navbar-brand" href="../public/index.php">Fragmentos de Loki</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
